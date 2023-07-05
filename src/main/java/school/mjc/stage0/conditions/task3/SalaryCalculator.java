03050562b9001cb7099e04794f65b65e1d4b5806
Sardor0305 <87496899+Sardor0305@users.noreply.github.com>
2023-07-05 17:58:24 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+                double taxRate;
+        if (salary < 0) {
+            System.out.println("wrong input!");
+            return;
+        } else if (salary <= 10000) {
+            taxRate = 0.15;
+        } else if (salary <= 20000) {
+            taxRate = 0.18;
+        } else {
+            taxRate = 0.2;
+        }
+        double netSalary = salary * (1 - taxRate);
+        System.out.println("Net Salary: " + netSalary);
     }
 }
